6073632d58e3483016189a97138ddd082cc49b65
Sesdras500 <sesdras500@gmail.com>
2024-08-28 21:09:53 -0300

    Create exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,11 @@
+# Passo 1: Solicitar ao usuário que digite a base do retângulo
+base = float(input("Digite a base do retângulo: "))
+
+# Passo 2: Solicitar ao usuário que digite a altura do retângulo
+altura = float(input("Digite a altura do retângulo: "))
+
+# Passo 3: Calcular a área do retângulo
+area = base * altura
+
+# Passo 4: Exibir a área do retângulo
+print(f"A área do retângulo é {area}")
